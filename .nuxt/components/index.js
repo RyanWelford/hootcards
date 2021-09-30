42b0a2cbd938e2cbe782553dc4d3a242bf11ba62
RyanWelford <ryan.welford@datastax.com>
2021-09-30 10:43:03 -0500

    build test

---
+++ .nuxt/components/index.js
@@ -0,0 +1,34 @@
+export { default as CardFlip } from '../../components/CardFlip.vue'
+export { default as HootCard } from '../../components/HootCard.vue'
+export { default as Pack } from '../../components/Pack.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
